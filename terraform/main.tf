2dc2e10d491e5747b5d8b97f6ba4255fd8206c40
Nicola Bicocchi <nbicocchi@unimore.it>
2025-10-22 18:06:29 +0200

    update

--- terraform/main.tf
+++ terraform/main.tf
@@ -47,7 +47,6 @@ resource "aws_instance" "product_service_instance" {
   instance_type               = var.instance_type
   key_name                    = aws_key_pair.deployment_key.key_name
   vpc_security_group_ids      = [aws_security_group.product_service_sg.id]
-  subnet_id                   = var.subnet_id
   associate_public_ip_address  = true
 
   tags = {
